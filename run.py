5a023259898724ac594141f9b1d3ea2bf4eff8e1
olugbenga akindele <akindelegbenga@gmail.com>
2024-02-05 07:02:22 -0700

    db installed only publication table created

--- run.py
+++ run.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.update(
+    SECRET_KEY = 'top_secret',
+    SQLALCHEMY_DATABASE_URI='postgresql://postgres:Flask123@localhost/Flaskapp',
+    SQLALCHEMY_TRACK_NOTIFICATION=False
+)
+
+db = SQLAlchemy(app)
+
+################################################################################
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("test.html")
 
 @app.route("/watch")
 def watch():
@@ -14,17 +24,27 @@ def watch():
                      "Tribe call Juda": 4.9,
 
                      }
-    return render_template("index.html"
-                           , title= "Movies"
-                           , movies = movies
-                           , dict_movie = dict_movie
-                           
-                           )
+    return render_template( "index.html" , title= "Movies" , movies = movies , dict_movie = dict_movie   )
 
 
 
 
+class Publication(db.Model):
+    __tablename__ = 'publication'
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(80), nullable = False)
+
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+
+    def __repr__(self):
+        return f"You have successfully created a new publication"
+    
 
 
 if __name__ =="__main__":
+    db.create_all()
     app.run(debug = True)
